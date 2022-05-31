4469e4c7b72886c2f74d3fe2125cd1c09f928da9
diegoescutia <106328707+diegoescutia@users.noreply.github.com>
2022-05-31 15:22:33 -0400

    Create Objective3Lab3.java

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,11 @@
+public class Objective3Lab3 {
+    public static void main(String[] args) {
+        int first = 1 + (int)(Math.random() * 6);
+        int second = 1 + (int)(Math.random() * 6);
+        int third = 1 + (int)(Math.random() * 6);
+        int solution = first + second + third;
+        
+        System.out.println(first + " + " + second + " + " + third + " = " + solution);
+       
+    }
+}
