761e0edb304d26b0ed6db9dac770afa72fce6a16
diegoescutia <106328707+diegoescutia@users.noreply.github.com>
2022-05-31 17:27:43 -0400

    Create Objective4Lab1.java

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+      Scanner keyboard = new Scanner(System.in);
+    
+System.out.println("What is your first name?");
+    String fname = input.nextLine();
+    System.out.println(fname);
+    
+System.out.println("What is your last name?");
+    String lname = input.nextLine();
+    System.out.println(lname);
+    
+System.out.println("What is your favorite animal?");
+    String favoriteAnimal = input.nextLine();
+    System.out.println(favoriteAnimal);
+    
+System.out.println("What is your favorite food?");
+    String favoriteFood = input.nextLine();
+    System.out.println(favoriteFood);
+    
+System.out.println("What is your favorite song?");
+    String favoriteSong = input.nextLine();
+    System.out.println(favoriteSong);
+    
+scanner.close();
+
+  }
+}
