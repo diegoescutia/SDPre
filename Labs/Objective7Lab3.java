93494d0d6b2b367be79ebd3274ce9d18e7a9fd8a
diegoescutia <106328707+diegoescutia@users.noreply.github.com>
2022-06-02 22:09:06 -0400

    Update Objective7Lab3.java

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -1,18 +1,19 @@
 public class Objective7Lab3 {
   public static void main(String[] args) {
-  int counter = 0;
+  int counter = 1;
     
     while(counter <= 20){
-      counter = counter + 1;
-      
-      if(counter % 2 == 0){
+        
+       if(counter % 2 == 0){
           System.out.println(counter + " is even");
       }
-     else if(counter % 2!= 0){System.out.println(counter + " is odd");}
+     else if(counter % 2!= 0){System.out.println(counter + " is odd");} 
+        
+      counter = counter + 1;
       
-       
     }
   }
 } 
+
 
 
