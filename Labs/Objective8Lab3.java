ef1f6e595899bad2507e3cf09017a21cd139e61e
diegoescutia <106328707+diegoescutia@users.noreply.github.com>
2022-06-06 14:25:59 -0400

    Create Objective8Lab3.java

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,16 @@
+public class Objective8Lab3 {
+    public static void main(String[] args) {
+        int counter = 0;
+        
+        for ( counter = 1; counter <= 20; counter++) {
+            
+            if (counter % 2 == 0) {
+               System.out.println(counter + " is even");}
+                
+            else if (counter % 2 != 0) {
+                System.out.println(counter +  " is odd");}
+            
+        }
+        
+    }
+}
